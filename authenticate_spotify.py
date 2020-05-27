cf9b2330e3b91b9c76b7956368d9de2b7f39ab7e
solomon-wheeler <63405769+solomon-wheeler@users.noreply.github.com>
2020-05-27 16:06:10 +0100

    Reformated files

--- authenticate_spotify.py
+++ authenticate_spotify.py
@@ -1,30 +1,30 @@
-# import sys
-import spotipy
 import spotipy.util as util
 import requests
 import json
 
+f = open("credentials.txt", "r")
+creds = []
+for x in f:
+    creds.append((x.strip()).split("=")[1])
+client_id = creds[0]
+client_secret = creds[1]
+dev_username = creds[2]
 
 
 def user_auth():
-    f = open("credentials.txt","r")
-    creds= []
-    for x in f:
-        creds.append((x.strip()).split("=")[1])
-    client_id = creds[0]
-    client_secret = creds[1]
-    dev_username = creds[2]
     scope = "user-top-read"
 
-    redirect = "http://localhost:8080"
-    user_token = util.prompt_for_user_token(username=dev_username,   #developer account username not client
+    redirect = "http://localhost:8080"  # Can be changed for others, this is just the port I chose, this has to be
+    # configured as the redirect url for spotify in the developer portal
+    user_token = util.prompt_for_user_token(username=dev_username,  # developer account username not client
                                             scope=scope,
                                             client_id=client_id,
                                             client_secret=client_secret,
                                             redirect_uri=redirect)
     return user_token
 
-#not used in this program
+
+# not used in this program
 def client_auth():
     grant_type = 'client_credentials'
     url = 'https://accounts.spotify.com/api/token'
@@ -34,4 +34,3 @@ def client_auth():
     client_token_raw = json.loads(response.text)
     client_token = client_token_raw["access_token"]
     return client_token
-
--- top_artists.py
+++ top_artists.py
@@ -3,54 +3,63 @@ import json
 from authenticate_spotify import user_auth
 import time
 
+
 def get_top_artists(user_token):
     url = "https://api.spotify.com/v1/me/top/artists"
     time_range = "medium_term"
     artist_limit = 50
-    headers = {"Authorization": "Bearer "+ user_token}
+    headers = {"Authorization": "Bearer " + user_token}
     payload = {"time_range": time_range, "limit": artist_limit}
-    spotify_response = requests.get(url, headers= headers, params = payload )
+    spotify_response = requests.get(url, headers=headers, params=payload)
     parsed_top_artists = json.loads(spotify_response.content)
     return parsed_top_artists
+
 
 def get_overall_popularity_data(top_artists):
     overall_popularity = 0
     amount_done = 0
     all_artist_data = []
     for individual_artist in top_artists['items']:
-        #print(individual_artist['popularity'])
+        # print(individual_artist['popularity'])
         overall_popularity += individual_artist['popularity']
-        amount_done += 1 # Always 50 at the moment but could be change in future
+        amount_done += 1  # Always 50 at the moment but could be change in future
         this_artist_data = [individual_artist['name'], individual_artist['popularity']]
         all_artist_data.append(this_artist_data)
-    average_popularity = int(overall_popularity/amount_done)
-    top_bot_artists = get_top_bot_5(all_artist_data,amount_done)
+    average_popularity = int(overall_popularity / amount_done)
+    top_bot_artists = get_top_bot_5(all_artist_data, amount_done)
     top_5_artists = top_bot_artists[1]
     bot_5_artists = top_bot_artists[0]
-    output_data(average_popularity,top_5_artists,bot_5_artists)
+    output_data(average_popularity, top_5_artists, bot_5_artists)
 
-def get_top_bot_5(all_artist_data,amount_done):
+
+def get_top_bot_5(all_artist_data, amount_done):
     top5_artists = []
     bot5_artists = []
-    sorted_all_artist_data = sorted(all_artist_data,key=lambda x: x[1])
-    for each_number in range(0,5):
+    sorted_all_artist_data = sorted(all_artist_data, key=lambda x: x[1])
+    for each_number in range(0, 5):
         bot5_artists.append(sorted_all_artist_data[each_number])
-        top5_artists.append(sorted_all_artist_data[amount_done - (each_number + 1)]) # need to check this works proparly
+        top5_artists.append(
+            sorted_all_artist_data[amount_done - (each_number + 1)])  # need to check this works proparly
     return top5_artists, bot5_artists
 
-def output_data(average_popularity,top_5_artists,bot_5_artists):
+
+def output_data(average_popularity, top_5_artists, bot_5_artists):
     print("I'm about to reveal you're score on the edgeometer ...")
     time.sleep(1)
     print("100 is the least edgey, and 0 is the most edgey")
     time.sleep(2)
 
-    print("Your score on the edge-ometer was " , average_popularity ," out of 100")
+    print("Your score on the edge-ometer was ", average_popularity, " out of 100")
     time.sleep(1)
 
-    edge_responses = [[90,"You need to get of the top 40 playlist , there's a world of music out there!" ],[80,"You didn't really know them before they were popular did you? "],
-                      [70,"A bit basic but respectable, you have the edge-o-meters approval" ], [60,"Very impressive, you're music is far of the beaten track" ],
-                      [50, "Wow someone's hip. Do you even know what capital is?" ], [40, "Your music taste is so far underground it's in china"],
-                      [30,"Really? You are clearly the definition of hipster, please teach me your ways"], [20,"Maybe there's a reason the music you listen to is so unpopular?"],
+    edge_responses = [[90, "You need to get of the top 40 playlist , there's a world of music out there!"],
+                      [80, "You didn't really know them before they were popular did you? "],
+                      [70, "A bit basic but respectable, you have the edge-o-meters approval"],
+                      [60, "Very impressive, you're music is far of the beaten track"],
+                      [50, "Wow someone's hip. Do you even know what capital is?"],
+                      [40, "Your music taste is so far underground it's in china"],
+                      [30, "Really? You are clearly the definition of hipster, please teach me your ways"],
+                      [20, "Maybe there's a reason the music you listen to is so unpopular?"],
                       [10, "be honest. Have you been listening to your own songs on spotify?"]]
     response_found = False
     responses_done = 0
@@ -64,31 +73,21 @@ def output_data(average_popularity,top_5_artists,bot_5_artists):
     print("The artists you listen to that are most edgy are:")
     time.sleep(1)
 
-    for each_in_list in range(0,5):
-        print(str(each_in_list + 1 ) + ". " + str(top_5_artists[each_in_list][0]) + " Popularity : " + str(top_5_artists[each_in_list][1]))
+    for each_in_list in range(0, 5):
+        print(str(each_in_list + 1) + ". " + str(top_5_artists[each_in_list][0]) + " Popularity : " + str(
+            top_5_artists[each_in_list][1]))
         time.sleep(0.5)
 
     print("The artists you listen to that are most popular are:")
     time.sleep(1)
 
-    for each_in_list in range(0,5):
-        print(str(each_in_list + 1 ) + ". " + str(bot_5_artists[each_in_list][0]) + " Popularity  : " + str(bot_5_artists[each_in_list][1]))
+    for each_in_list in range(0, 5):
+        print(str(each_in_list + 1) + ". " + str(bot_5_artists[each_in_list][0]) + " Popularity  : " + str(
+            bot_5_artists[each_in_list][1]))
         time.sleep(0.5)
-
-
-
-
-
-
-
-
-
-
 
 
 ##main##
 user_token = user_auth()
 top_artists = get_top_artists(user_token)
-#print(json.dumps(top_artists,indent=4, sort_keys=True))
-#print(top_artists)
 get_overall_popularity_data(top_artists)
